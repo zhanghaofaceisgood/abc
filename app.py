fe0515934735438d6364ea1885c855eae8bb8df2
zhanghaofaceisgood <imbutterwaffle@gmail.com>
2025-11-15 11:40:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ name = st.text_input('이름')
 
 if st.button('안녕하세요'):
     st.write(f'안녕하세요, {name}님 ! !')
+
+st.markdown(
+    """
+    <style>
+        /* 전체 배경색 변경 */
+        .stApp {
+            background-color: #ffe6f2; /* 연한 핑크 */
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
