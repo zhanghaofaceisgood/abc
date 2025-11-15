4fc8879b8a3cf32e7f5f94cee547cc7183435b53
zhanghaofaceisgood <imbutterwaffle@gmail.com>
2025-11-15 11:46:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown(
     <style>
         /* 전체 배경색 변경 */
         .stApp {
-            background-color: #ffco00; /* 연한 핑크 */
+            background-color: #F7C6D5; /* 연한 핑크 */
         }
     </style>
     """,
