42282f85c008ff1784983a14d3f7565736012de3
zhanghaofaceisgood <imbutterwaffle@gmail.com>
2025-11-15 11:35:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('‧₊˚⋅♡⋅˚₊‧')
 
-name = st.text_input('이름을 입력하세요:')
+name = st.text_input('이름')
 
 if st.button('안녕하세요'):
-    st.write(f'안녕하세요, {name}님!')
+    st.write(f'안녕하세요, {name}님 ! !')
